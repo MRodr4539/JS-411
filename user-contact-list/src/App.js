47f0f0504ac93554bbff2ab9d614573664b342e7
MRodr4539 <miranda.rodriguez4539@yahoo.com>
2019-11-02 13:46:56 -0500

    button working!

--- user-contact-list/src/App.js
+++ user-contact-list/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import './App.css';
 
 
-
-
 class App extends Component {
   constructor(props){
     super(props);
 
     this.state = {
       results: [],
+      details: '',
+      showHide : false,
     };
+    this.toggle = this.toggle.bind(this)
+  }
+
+  toggle = () => {
+    const { showHide } = this.state;
+    this.setState({showHide: !this.state.showHide})
   }
 
   componentDidMount(){
@@ -24,19 +30,28 @@ class App extends Component {
       this.setState({results: data.results})
     })
   }
+  
 
   render(){
     console.log(this.state.results)
     return(
       <div className= 'User-Contact-List'>
         <h1>Contact List</h1>
-       
+        <button onClick={this.toggle}>{this.state.showHide ? 'Hide Details' : 'Show Details'}</button>
+        { this.state.showHide && <Details />}
       </div>
     );
   } 
 
 }
 
+class Details extends Component {
+  render(){
+    return(
+      <div>User Details</div>
+    )
+  }
+}
 
 
 
