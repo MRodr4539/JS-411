144b00466ea4592f86a06e52f40b5a6fe0d58d2b
MRodr4539 <miranda.rodriguez4539@yahoo.com>
2019-11-05 17:35:49 -0600

    all together

--- user-contact-list/src/App.js
+++ user-contact-list/src/App.js
@@ -3,55 +3,124 @@ import './App.css';
 
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
     this.state = {
       results: [],
-      details: '',
-      showHide : false,
+      showHide: false
     };
-    this.toggle = this.toggle.bind(this)
+
+    this.toggle = this.toggle.bind(this);
   }
 
   toggle = () => {
-    const { showHide } = this.state;
-    this.setState({showHide: !this.state.showHide})
-  }
+    // const { showHide } = this.state;
+    this.setState({ showHide: !this.state.showHide });
+    this.setState({ title: !this.state.Info });
+  };
 
-  componentDidMount(){
+  componentDidMount() {
     this.fetchData();
-  }  
-
-  fetchData(){
-    fetch('https://randomuser.me/api?results=25')
-    .then(response => response.json())
-    .then(data => {
-      this.setState({results: data.results})
-    })
   }
-  
 
-  render(){
-    console.log(this.state.results)
-    return(
-      <div className= 'User-Contact-List'>
+  fetchData() {
+    fetch("https://randomuser.me/api?results=25")
+      .then(response => response.json())
+      .then(data => {
+        console.log("Data fetched", data);
+        this.setState({ results: data.results });
+      });
+  }
+
+  render() {
+    // console.log("Rendered data", this.state.results);
+    // console.log("Attemping details pull", this.state.detail);
+    // const { results } = this.state;
+    return (
+      <div className="User-Contact-List">
         <h1>Contact List</h1>
-        <button onClick={this.toggle}>{this.state.showHide ? 'Hide Details' : 'Show Details'}</button>
-        { this.state.showHide && <Details />}
+        {this.state.results.map((object, index) => (
+          <p key={index}>
+            Username: {object.login.username}
+            <br />
+            <img src={object.picture.large} />
+            <br />
+            <button onClick={this.toggle}>
+              {this.state.showHide ? "Hide Details" : "Show Details"}
+            </button>
+            <br />
+            {this.state.showHide && object.name.title}
+            {this.state.showHide && object.name.first}
+            {this.state.showHide && object.name.last}
+          </p>
+        ))}
       </div>
     );
-  } 
-
-}
-
-class Details extends Component {
-  render(){
-    return(
-      <div>User Details</div>
-    )
   }
 }
+
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+
+//     this.state = {
+//       results: [],
+//       details: '',
+//       showHide : false,
+//     };
+//     this.toggle = this.toggle.bind(this)
+//   }
+
+//   toggle = () => {
+//     const { showHide } = this.state;
+//     this.setState({showHide: !this.state.showHide})
+//   }
+
+//   componentDidMount(){
+//     this.fetchData();
+//   }  
+
+//   fetchData(){
+//     fetch('https://randomuser.me/api?results=25')
+//     .then(response => response.json())
+//     .then(data => {
+//       this.setState({
+//         results: data.results,
+        
+//       })
+//     })
+//   }
+  
+
+//   render(){
+//     // console.log('this is state', this.state.results)
+//     const { results } = this.state;
+//     return(
+//       <div className= 'User-Contact-List'>
+//         <h1>Contact List</h1>
+//         <ul>
+//           {results.map(result =>
+//           <li key={result.objectID}>
+//             <a href={result.url}>{result.username}</a>
+//           </li>
+//           )}
+//         </ul>
+//         <button onClick={this.toggle}>{this.state.showHide ? 'Hide Details' : 'Show Details'}</button>
+//         { this.state.showHide && <Details />}
+//       </div>
+//     );
+//   } 
+
+// }
+
+// class Details extends Component {
+//   render(){
+//     return(
+//       <div>User Details</div>
+//     )
+//   }
+// }
 
 
 
